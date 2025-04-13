fcdae35251906bd32c02a6412db3ff76bdadceaf
Alperen Yilmazyildiz <46990789+AlperenYilmazyildiz@users.noreply.github.com>
2025-04-13 22:27:57 +0300

    .

--- tests/test1.js
+++ tests/test1.js
@@ -4,8 +4,8 @@ const capabilities = {
   platformName: 'Android',
   'appium:automationName': 'UiAutomator2',
   'appium:deviceName': 'Android',
-  'appium:appPackage': 'com.anonymous.CS458Project2', // Replace with your app's package name
-  'appium:appActivity': '.MainActivity', // Replace with your app's main activity 'appium:autoGrantPermissions': true,
+  'appium:appPackage': 'com.anonymous.CS458Project2', 
+  'appium:appActivity': '.MainActivity', 
   'appium:app': './android/app/build/outputs/apk/debug/app-debug.apk',
   'appium:noReset': false
 };
